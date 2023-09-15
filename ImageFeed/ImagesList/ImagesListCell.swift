7de231da1f021d842c0b42447826bee828dde36e
DariaL <yellow.danielle@gmail.com>
2023-09-15 13:58:15 +0400

    sprint 8

---
+++ ImageFeed/ImagesList/ImagesListCell.swift
@@ -0,0 +1,18 @@
+//
+//  ImagesListCell.swift
+//  ImageFeed
+//
+//  Created by Daria Lovkova on 14.09.2023.
+//
+
+import Foundation
+import UIKit
+
+final class ImagesListCell: UITableViewCell {
+    static let reuseIdentifier = "ImagesListCell"
+    @IBOutlet weak var cellImage: UIImageView!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var likeButton: UIButton!
+    
+}
